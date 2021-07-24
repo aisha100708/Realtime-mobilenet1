8125bec634b352b3c7650c1892c1e27a33ec8a47
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-07-24 18:03:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+function setup() {
+  canvas = createCanvas(600, 500);
+  canvas.position(650, 370);
+  video = createCapture(VIDEO);
+  video.hide();
+
+}
+function draw() {
+
+}
+
+
