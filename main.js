cb2d47f9dd8176f2e754393984017514ed80cb02
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-08-07 17:40:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,22 @@ function setup() {
   canvas.position(650, 370);
   video = createCapture(VIDEO);
   video.hide();
-
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
 function draw() {
-
+  image(video, 0, 0, 600, 500);
+  classifier.classify(video, getResults);
 }
-
-
+function modelLoaded() {
+  console.log("model loaded!");
+}
+function getResults(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  else {
+     console.log(results);
+     document.getElementById("object_name").innerHTML = results[0].label;
+     document.getElementById("accuracy_percentage").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
